52384a99877f2bbcb33decc75e4fbc4d2f650e97
AnnieTheAvatar <annielacoba@yahoo.com>
2025-03-19 20:28:43 +1100

    amplify support

---
+++ adp-2025-testgrad/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "adpgrad2025": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "adp2025testgradf8aa0f07": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
