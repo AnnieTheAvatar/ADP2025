576e660b73d7e55d6e5cce8b4cb1600e2ed386c5
AnnieTheAvatar <annielacoba@yahoo.com>
2025-03-19 21:51:06 +1100

    next config

--- adp-2025-testgrad/next.config.mjs
+++ adp-2025-testgrad/next.config.mjs
@@ -2,7 +2,10 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  output: 'standalone', // Ensure Amplify serves the correct build
+  experimental: {
+    appDir: true, // Explicitly enable App Router
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
